9197814399ee19b7a7d55784be388e366086d781
tuanle277 <letuanminh2707@gmail.com>
2025-03-30 16:33:56 -0500

    ver 1

--- src/server/api/routers/llama.ts
+++ src/server/api/routers/llama.ts
@@ -28,7 +28,7 @@ export const llamaRouter = createTRPCRouter({
             {
               role: "user",
               content:
-                "Act as an OCR assistant. Analyze the provided image and:\n1. Recognize all visible text in the image as accurately as possible.\n2. Maintain the original structure and formatting of the text.\n3. If any words or phrases are unclear, indicate this with [unclear] in your transcription.\nProvide only the transcription without any additional comments.",
+                "Act as an OCR assistant. Analyze the provided image and:\n1. Recognize all visible text in the image as accurately as possible.\n2. Maintain the original structure and formatting of the text.\n3. If any words or phrases are unclear, indicate this with [unclear] in your transcription.\nProvide only the transcription without any additional comments. Then Summarize this document, until the next time I see my doctor, give me some tips/actionables to improve and maintain my health.",
               images: [base64Image],
             },
           ],
@@ -49,6 +49,39 @@ export const llamaRouter = createTRPCRouter({
       console.log(4);
       const ocrText = result.message?.content ?? "";
       console.log(ocrText);
+
+      // const prompt = "";
+
+      // const llamaResponse2 = await fetch("http://localhost:11434/api/chat", {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({
+      //     model: "llama3",
+      //     messages: [
+      //       {
+      //         role: "user",
+      //         content: prompt,
+      //       },
+      //       {
+      //         role: "user",
+      //         content: ocrText,
+      //       },
+      //     ],
+      //     stream: false,
+      //     TIMEOUT: 10000,
+      //   }),
+      // });
+
+      // const result2 = await llamaResponse2.json();
+
+      // console.log(4);
+      // const ocrText2 = result.message?.content ?? "";
+      // console.log("__________________________");
+      // console.log(ocrText2);
+
+      // // ocrText2 is the goat now 
 
       const document = await ctx.db
         .insert(healthcareDocuments)
